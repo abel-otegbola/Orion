495545622176dc6c8329b02b88ccf31046b977c8
ABEL OTEGBOLA <abel15655@gmail.com>
2024-10-16 21:58:52 +0100

    feat: add footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer/footer";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         className={`${inter.className} antialiased dark:bg-dark dark:text-white/[0.9]`}
       >
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,34 +4,32 @@ import { Apple01Icon, Facebook01Icon, InstagramIcon, MasterCardIcon, PaypalIcon,
 
 function Footer() {
     return (
-        <footer className="bg-black dark:bg-[#000]/[0.2] text-[#D9D9F2]">
-            <div className="grid grid-cols-2 text-[12px] gap-[30px] py-[30px] md:px-[9%] px-[3%] border border-transparent border-t-gray-700/[0.09] dark:border-t-gray-100/[0.09]">
+        <footer className="text-[#D9D9F2] mt-8">
+            <div className="bg-[#000]/[0.95] flex flex-col items-center gap-[30px] py-[30px] md:px-[9%] px-[3%] pt-[60px] border border-transparent border-t-gray-700/[0.09] dark:border-t-gray-100/[0.09]">
+                <h1 className="text-[32px]">Next js boilerplate</h1>
                 <ul className="w-full">
-                    <h2 className="py-2 font-semibold uppercase">Navigations</h2>
-                    <li className="flex w-full"><a href="/shop?query=Sweatshirts" className="py-[5px] w-full">Home</a></li>
-                    <li className="flex w-full"><a href="/shop?query=academics" className="py-[5px] w-full">About</a></li>
-                    <li className="flex w-full"><a href="/shop?query=joggers" className="py-[5px] w-full">Shop</a></li>
-                    <li className="flex w-full"><a href="/shop?query=hoddies" className="py-[5px] w-full">Contact Us</a></li>
-                </ul>
-                <ul className="w-full">
-                    <div className="flex flex-wrap gap-4 py-4 text-">
-                        <a href="https://facebook.com/"><Facebook01Icon /></a>
-                        <a href="https://twitter.com/"><TwitterIcon /></a>
-                        <a href="https://instagram.com/"><InstagramIcon /></a>
+                    <div className="flex flex-wrap gap-4 py-2 mb-4 justify-center">
+                        <a href="https://facebook.com/" className="p-4 border border-gray-500/[0.4] rounded-full"><Facebook01Icon /></a>
+                        <a href="https://twitter.com/" className="p-4 border border-gray-500/[0.4] rounded-full"><TwitterIcon /></a>
+                        <a href="https://instagram.com/" className="p-4 border border-gray-500/[0.4] rounded-full"><InstagramIcon /></a>
                     </div>
-                    <p className="py-1">Agege, Lagos, Nigeria</p>
-                    <a href="tel:+2347060989331" className="block py-1">+2348160301851</a>
-                    <a href="mailto:suppercaseort@ennovate.com" className="block py-1">Support@boilerplate.com</a>
-                    <div className="flex flex-wrap gap-4 py-4 text-[25px]">
+                    <a href="mailto:suppercaseort@ennovate.com" className="block py-1 text-center">Support@boilerplate.com</a>
+                    <div className="flex flex-wrap justify-center gap-4 py-4 text-[25px]">
                         <PaypalIcon className="text-blue-500"/>
                         <MasterCardIcon className="text-[#FF5F00]"/>
                         <VineSquareIcon className="text-[#1434CB]"/>
                         <Apple01Icon />
                     </div>
                 </ul>
+                <ul className="flex gap-[40px] justify-center">
+                    <li className=""><a href="/" className="py-[5px] w-full">Home</a></li>
+                    <li className=""><a href="/about" className="py-[5px] w-full">About</a></li>
+                    <li className=""><a href="/shop" className="py-[5px] w-full">Shop</a></li>
+                    <li className=""><a href="/contact" className="py-[5px] w-full">Contact Us</a></li>
+                </ul>
             </div>
-            <div className="bg-[#000]/[0.2] text-white text-center">
-                <p className="px-[3%] py-3 flex items-center gap-2 justify-center">Nextjs boilerplate &copy; Copyright  {new Date().getFullYear()}</p>
+            <div className="bg-[#000] text-center">
+                <p className="px-[3%] py-10 flex items-center gap-2 justify-center">Nextjs boilerplate &copy; Copyright  {new Date().getFullYear()}</p>
             </div>
         </footer>
     )
